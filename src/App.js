1fdd1959baa4e814d502e03da9067157040697a9
veena1994-hash <79410385+veena1994-hash@users.noreply.github.com>
2021-05-12 20:32:23 -0400

    product component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products:data.products,
+      size:"",
+      sort:"",
+    };
+  }
 
-function App() {
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">React shopping cart</a>
       </header>
-      <main>Productlist</main>
+      <main>
+        <div className="content">
+          <div className="main">
+          <Products products={this.state.products}></Products>
+        </div>
+        <div className="sidebar">cart items</div>
+        </div>
+      </main>
       <footer>All right is reserved</footer>
     </div>
 
-  );
+  )};
 }
 
 export default App;
